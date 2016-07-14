9387228f4f9fa13c5daef1775d3e09a91e7512fe
RamonSchmitt <ramon.schmitt@yahoo.com>
2016-07-14 11:39:40 +0200

    timer

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-class Timer extends React.Component {
-    render() {
-        return (
-          <div>
-            <h1>Timer</h1>
-          </div>
-        );
+var Timer = React.createClass({
+  getInitialState: function() {
+    return {secondsRemaining: 20};
+  },
+
+  tick: function() {
+    this.setState({secondsRemaining: this.state.secondsRemaining - 1});
+    if (this.state.secondsRemaining <= 0) {
+      clearInterval(this.interval);
     }
-}
+  },
+
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, 1000);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+
+  render: function() {
+    return (
+      <div>Time Remaining: {this.state.secondsRemaining}</div>
+    );
+  }
+});
 
 export default Timer;
